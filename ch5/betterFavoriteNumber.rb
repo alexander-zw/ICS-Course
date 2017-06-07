e1766d7b2c5ee7ce284c61c2e2d175ae3758b189
alexander-zw <alexanderwu68@yahoo.com>
2017-06-07 16:05:02 +0800

    Update betterFavoriteNumber.rb to screen input

--- ch5/betterFavoriteNumber.rb
+++ ch5/betterFavoriteNumber.rb
@@ -2,5 +2,17 @@
 # as a bigger, better favorite number
 
 puts 'Hey, old sport. What\'s your favorite number?'
-favoriteNum = gets.to_i
-puts 'Well, I have an idea. Isn\'t ' + (favoriteNum + 1).to_s + ' a bigger, better favorite number?'
+
+while true
+  strFavoriteNum = gets.chomp
+
+  # Check if it's an integer
+  if strFavoriteNum[/[0-9]+/] == strFavoriteNum
+    break
+  else
+    puts 'I\'m sorry, it has to be a positive integer. Maybe you can tell me again:'
+  end
+
+end
+
+puts 'Well, I have an idea. Isn\'t ' + (strFavoriteNum.to_i + 1).to_s + ' a bigger, better favorite number?'
