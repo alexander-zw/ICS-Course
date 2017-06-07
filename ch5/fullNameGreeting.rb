9e94abf2a8434e178ccc928ec881bad13112ec1e
alexander-zw <alexanderwu68@yahoo.com>
2017-06-07 15:41:18 +0800

    Update fullNameGreeting.rb to screen input with duplicate code

--- ch5/fullNameGreeting.rb
+++ ch5/fullNameGreeting.rb
@@ -3,9 +3,124 @@
 
 puts 'Hey there. What\'s your first name?'
 wholeName = gets.chomp
+
+# Check if the name consists of only letters, is capitalized, and is not empty
+invalidName = true
+while invalidName
+
+  if wholeName[/[A-Za-z]+/] != wholeName || wholeName.capitalize != wholeName
+
+    puts 'Are you sure that\'s your name?'
+    reply = gets.chomp
+    while true
+
+      if reply.downcase == 'yes'
+        puts 'Okay, if you\'re sure.'
+        invalidName = false
+        break # break from both loops
+
+      elsif reply.downcase == 'no'
+        puts 'Then you\'d better tell me again.'
+        wholeName = gets.chomp
+        break # break only from inner loop
+
+      else
+        puts 'Huh? Please type "yes" or "no":'
+        reply = gets.chomp # don't break from either loop
+
+      end
+
+    end
+
+  else
+    break # Name passes test
+  end
+end
+
 puts 'What about your middle name? (If you don\'t have one, just press enter.)'
-wholeName += ' ' + gets.chomp
+middleName = gets.chomp
+
+# Check if the name consists of only letters and is capitalized
+invalidName = true
+while invalidName
+
+  if (middleName[/[A-Za-z]+/] != middleName || middleName.capitalize != middleName) && middleName != ''
+
+    puts 'Are you sure that\'s your name?'
+    reply = gets.chomp
+    while true
+
+      if reply.downcase == 'yes'
+        puts 'Okay, if you\'re sure.'
+        invalidName = false
+        break # break from both loops
+
+      elsif reply.downcase == 'no'
+        puts 'Then you\'d better tell me again.'
+        middleName = gets.chomp
+        break # break only from inner loop
+
+      else
+        puts 'Huh? Please type "yes" or "no":'
+        reply = gets.chomp # don't break from either loop
+
+      end
+
+    end
+
+  else
+    break # Name passes test
+  end
+end
+
+# Add a space if there is a middle name and a first name
+if middleName != '' && wholeName != ''
+  wholeName += ' ' + middleName
+else
+  wholeName += middleName
+end
+
 puts 'And if you don\'t mind, your last name?'
-wholeName += ' ' + gets.chomp
+lastName = gets.chomp
+
+# Check if the name consists of only letters, is capitalized, and is not empty
+invalidName = true
+while invalidName
+
+  if lastName[/[A-Za-z]+/] != lastName || lastName.capitalize != lastName
+
+    puts 'Are you sure that\'s your name?'
+    reply = gets.chomp
+    while true
+
+      if reply.downcase == 'yes'
+        puts 'Okay, if you\'re sure.'
+        invalidName = false
+        break # break from both loops
+
+      elsif reply.downcase == 'no'
+        puts 'Then you\'d better tell me again.'
+        lastName = gets.chomp
+        break # break only from inner loop
+
+      else
+        puts 'Huh? Please type "yes" or "no":'
+        reply = gets.chomp # don't break from either loop
+
+      end
+
+    end
+
+  else
+    break # Name passes test
+  end
+end
+
+# Add a space if there is a last name and there is a middle or first name
+if lastName != '' && wholeName != ''
+  wholeName += ' ' + lastName
+else
+  wholeName += lastName
+end
 
 puts 'Okay, nice to meet you, ' + wholeName + '.'
