3c612374eb271d8e3d0d689f2bfb8b615f760b4f
alexander-zw <alexanderwu68@yahoo.com>
2017-06-20 16:02:42 +0800

    Create sort.rb in ch10

---
+++ ch10/sort.rb
@@ -0,0 +1,124 @@
+# Asks for input strings, sorts them with recursion, and outputs them
+# Note: All characters are accepted. Strings are sorted according to unicode.
+
+# --------------------------- METHOD OF SORTING ---------------------------
+
+# wrapper method that sorts arr
+def sort arr
+  return recursiveSort(arr, [])
+end
+
+# recursive helper method that sorts arr
+def recursiveSort unsorted, sorted
+
+  # check for termination condition
+  if unsorted.empty?
+    return sorted
+  end
+
+  # find minimum index
+  minI = 0
+  unsorted.each_with_index do |word, i|
+    if word < unsorted[minI]
+      minI = i
+    end
+  end
+  # put min word from unsorted to sorted
+  sorted.push(unsorted[minI])
+  unsorted.delete_at(minI)
+  # sort the rest
+  return recursiveSort(unsorted, sorted)
+
+end
+
+# --------------------------- USER INTERFACE ---------------------------
+
+# displays a list of strings in a fancy fashion
+def printStrings strArr, maxLength
+  strArr.each_with_index do |str, i|
+    # center strings, add spaces on left to add shifting effect
+    puts ' ' * i * 2 + str.center(maxLength)
+  end
+end
+
+# prints the example array of strings and returns an array
+# containing the example array and its maximum string length
+def showExampleArr
+  strings = ['Mike', 'x-ray', 'ABC', '1,000,000', 'NASCAR', '你好', 'zebra',
+           '"The Force is strong with this one."', 'éclairs', 'milk', '???',
+           'a merry Christmas', 'Ruby']
+  maxLength = '"The Force is strong with this one."'.length
+  puts 'This is a list of "words":'
+  printStrings(strings, maxLength)
+  puts '(Please press enter)'
+  gets
+  return [strings, maxLength]
+end
+
+# ask whether user wants to use predetermined list
+def askForExample
+
+  puts 'Would you like to use the example list of strings?'
+  while true
+    reply = gets.chomp
+
+    if reply.downcase == 'yes'
+      puts 'Okay, here is the list:'
+      return true
+
+    elsif reply.downcase == 'no'
+      puts 'Then you may tell me what you want to sort.'
+      return false
+
+    else # don't break from loop
+      puts 'Huh? Please type "yes" or "no":'
+
+    end
+  end
+
+end
+
+# asks for strings and returns an array containing the string array
+# and the maximum string length
+def getStrings
+
+  strings = []
+  puts 'Please input the strings one by one and press enter to exit:'
+
+  # keep track of maximum length of strings
+  maxLength = 0
+
+  # obtain strings
+  while true
+
+    str = gets.chomp
+    if str == ''
+      break
+    end
+    strings.push(str)
+
+    if str.length > maxLength
+      maxLength = str.length
+    end
+
+  end
+
+  return [strings, maxLength]
+
+end
+
+# --------------------------- COMPLETE PROCEDURE ---------------------------
+
+# obtain string array, whether it is predetermined or user-chosen
+if askForExample
+  stringsAndMaxLen = showExampleArr
+else
+  stringsAndMaxLen = getStrings
+end
+
+# sort the array
+strings = sort(stringsAndMaxLen[0])
+
+# print the array
+puts 'After sorting, the strings are:'
+printStrings(strings, stringsAndMaxLen[1])
